d976800706b22b3a29d6891eab77fb9ca07cf222
Andy Sanchez <andy62800leo@gmail.com>
2025-09-24 20:47:52 -0700

    Exercise 2 and 3 completed

--- ex02.js
+++ ex02.js
@@ -1,3 +1,6 @@
 // Find Exercise
 // Instructions: Create an array of at least five numbers. Write code to find the first element that is greater than 10.
 
+const numbers = [3, 22, 33, 42, 58, 11, 39, 7, 91]
+const found = numbers.find(numbers => numbers > 10)
+console.log(found)
--- ex03.js
+++ ex03.js
@@ -1,3 +1,6 @@
 // Reverse Exercise
 // Instructions: Create an array of at least five numbers. Write code to reverse the order of the elements in the array.
 
+const numbers = [35, 132, 43, 97, 85, 23]
+console.log(`original numbers: ${numbers.join(", ")}`)
+console.log(`reversed numbers: ${numbers.toReversed().join(", ")}`)

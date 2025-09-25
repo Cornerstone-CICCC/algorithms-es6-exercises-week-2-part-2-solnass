ce09261e51a297a6250e3cfae103f0f9bfe7eda5
Andy Sanchez <andy62800leo@gmail.com>
2025-09-24 21:03:22 -0700

    Exercise 4 and 8 completed

--- ex04.js
+++ ex04.js
@@ -1,3 +1,9 @@
 // Includes Exercise
 // Instructions: Create an array of at least five different fruits as strings. Write code to check if the array includes the fruit "apple".
 
+const wildBerries = ["plum", "blueberry", "grape", "sloes", "strawberry", "cherry"]
+if (wildBerries.includes("apple")){
+    console.log("Apple is here! n.n")
+} else {
+    console.log("There is no apple here!")
+}
--- ex08.js
+++ ex08.js
@@ -1,3 +1,5 @@
 // Join Exercise
 // Instructions: Create an array of at least five different words as strings. Write code to join all the words into a single string, with each word separated by a space.
 
+const exercise = ["This", "phrase", "was", "created", "using", "join", "array!"]
+console.log(exercise.join(" "))
